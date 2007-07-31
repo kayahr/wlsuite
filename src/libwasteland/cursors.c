4566ba344e2d9717b81fd3c12975114099b0c64c
Klaus Reimer <k@ailis.de>
2007-07-31 21:04:03 +0000

    Implemented cursors support

---
+++ src/libwasteland/cursors.c
@@ -0,0 +1,199 @@
+/*
+ * $Id$
+ * Copyright (C) 2007  Klaus Reimer <k@ailis.de>
+ * See COPYING file for copying conditions
+ */
+
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include "wasteland.h"    
+
+/**
+ * Reads cursors from the specified file and returns it as a an array of
+ * images. You have to release the allocated memory of this array when you
+ * no longer need it. If an error occurs while reading the source file then
+ * NULL is returned and you can use errno to find the reason.
+ * 
+ * A pixel in the returned array can be accessed like this:
+ * cursors[cursorNo][y * 16 + x]. A pixel is an integer between 0 and 255. The
+ * lower 4 bits is the EGA color palette index. The higher 4 bits are the
+ * transparency bits for the lower 4 bit. So if bit 4 is set then this
+ * means that bit 0 (The blue component) is transparent. This feature is not
+ * used in the original wasteland cursors. These original cursors only have
+ * fully solid pixels or fully transparent pixels (Which means that all high
+ * bits are set if the pixel is transparent or they are cleared if the pixel
+ * is solid). If you don't need sub-pixel-transparency either then you can
+ * interpret the value like this: If the pixel value is lower than 16 then the
+ * pixel value is simply the EGA color index. If the value is higher or equal 
+ * 16 then the pixel is transparent.
+ *
+ * @param filename
+ *            The filename of the cursor file to read
+ * @return The cursors as an array of images
+ */
+
+wlImages wlCursorsReadFile(char *filename)
+{
+    FILE *file;
+    wlImages cursors;
+    
+    assert(filename != NULL);
+    file = fopen(filename, "rb");
+    if (!file) return NULL;
+    cursors = wlCursorsReadStream(file);
+    fclose(file);
+    return cursors;
+}
+
+
+/**
+ * Reads sprites from the specified file stream. The stream must already be
+ * open and pointing to the cursors data. The stream is not closed by this 
+ * function so you have to do this yourself.
+ * 
+ * You have to release the allocated memory of the returned array when you
+ * no longer need it. If an error occurs while reading the source streams then
+ * NULL is returned and you can use errno to find the reason.
+ * 
+ * A pixel in the returned array can be accessed like this:
+ * cursors[cursorNo][y * 16 + x]. A pixel is an integer between 0 and 255. The
+ * lower 4 bits is the EGA color palette index. The higher 4 bits are the
+ * transparency bits for the lower 4 bit. So if bit 4 is set then this
+ * means that bit 0 (The blue component) is transparent. This feature is not
+ * used in the original wasteland cursors. These original cursors only have
+ * fully solid pixels or fully transparent pixels (Which means that all high
+ * bits are set if the pixel is transparent or they are cleared if the pixel
+ * is solid). If you don't need sub-pixel-transparency either then you can
+ * interpret the value like this: If the pixel value is lower than 16 then the
+ * pixel value is simply the EGA color index. If the value is higher or equal 
+ * 16 then the pixel is transparent.
+ * 
+ * @param stream
+ *            The stream to read cursors from
+ * @return The cursors as an array of images
+ */
+
+wlImages wlCursorsReadStream(FILE *stream)
+{
+    wlImages cursors;
+    int cursor, bit, x, y, type, pixel, b;
+    
+    assert(stream != NULL);
+    cursors = (wlImages) malloc(8 * sizeof(wlImage));
+    for (cursor = 0; cursor < 8; cursor++)
+    {
+        cursors[cursor] = (wlImage) malloc(16 * 16 * sizeof(wlPixel));
+        
+        for (bit = 0; bit < 4; bit++)
+        {
+            for (y = 0; y < 16; y++)
+            {
+                for (type = 0; type < 2; type++)
+                {
+                    for (x = 8; x >= 0; x -= 8)
+                    {
+                        b = fgetc(stream);
+                        if (b == EOF) return NULL;
+                        for (pixel = 0; pixel < 8; pixel++)
+                        {
+                            if (type)
+                            {
+                                cursors[cursor][y * 16 + x + pixel] |=
+                                    ((b >> (7 - pixel)) & 1) << bit;
+                            }
+                            else
+                            {
+                                cursors[cursor][y * 16 + x + pixel] |=
+                                    !((b >> (7 - pixel)) & 1) << (4 + bit);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return cursors;
+}
+
+
+/**
+ * Writes cursors to a file. The function returns 1 if write was successfull
+ * and 0 if write failed. In this case you can read the reason from errno.
+ *
+ * @param cursors
+ *            The cursors to write (Array with 8 16x16 images)
+ * @param filename
+ *            The filename of the file to write the cursors to
+ * @return 1 on success, 0 on failure
+ */
+
+int wlCursorsWriteFile(wlImages cursors, char *filename)
+{
+    FILE *file;
+    int result;
+    
+    assert(cursors != NULL);
+    assert(filename != NULL);
+    file = fopen(filename, "wb");
+    if (!file) return 0;
+    result = wlCursorsWriteStream(cursors, file);
+    fclose(file);
+    return result;
+}
+
+
+/**
+ * Writes curors to a stream. The stream must already be open and pointing
+ * to the location where you want to write the cursors to. The stream is not
+ * closed by this function so you have to do this yourself. The function
+ * returns 1 if write was successfull and 0 if write failed. In
+ * this case you can read the reason from errno.
+ *
+ * @param cursors
+ *            The cursors to write (Array with 8 16x16 images)
+ * @param stream
+ *            The stream to write the cursors to
+ * @return 1 on success, 0 on failure
+ */
+
+int wlCursorsWriteStream(wlImages cursors, FILE *stream)
+{
+    int cursor, bit, y, type, x, pixel, b;
+ 
+    assert(cursors != NULL);
+    assert(stream != NULL);
+    for (cursor = 0; cursor < 8; cursor++)
+    {
+        for (bit = 0; bit < 4; bit++)   
+        {
+            for (y = 0; y < 16; y++)
+            {
+                for (type = 0; type < 2; type++)
+                {
+                    for (x = 8; x >= 0; x -= 8)
+                    {
+                        b = 0;
+                        for (pixel = 0; pixel < 8; pixel++)
+                        {
+                            if (type)
+                            {
+                                b |= ((cursors[cursor][y * 16 + x + pixel]
+                                    >> bit) & 0x01) << (7 - pixel);
+                            }
+                            else
+                            {
+                                b |= !((cursors[cursor][y * 16 + x + pixel]
+                                    >> (4 + bit)) & 0x01) << (7 - pixel);
+                            }
+                        }
+                        if (fputc(b, stream) == EOF) return 0;
+                    }
+                }
+            }
+        }
+    }
+    return 1;
+}
---
+++ src/unpackcursors/unpackcursors.c
@@ -0,0 +1,237 @@
+/*
+ * $Id$
+ * Copyright (C) 2007  Klaus Reimer <k@ailis.de>
+ * See COPYING file for copying conditions
+ */
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <getopt.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <string.h>
+#include <gd.h>
+#include <errno.h>
+#include <wasteland.h>
+#include "config.h"
+
+#ifdef WIN32
+#define SEPARATOR '\\'
+#else
+#define SEPARATOR '/'
+#endif
+
+
+/**
+ * Displays the usage text.
+ */
+
+static void display_usage(void) 
+{
+    printf("Usage: wl_unpackcursors [OPTION]... CURSORSFILE OUTPUTDIR\n");
+    printf("Unpacks cursors into PNG images.\n");
+    printf("\nOptions\n");
+    printf("  -h, --help              Display help and exit\n");
+    printf("  -V, --version           Display version and exit\n");
+    printf("\nReport bugs to %s <%s>\n", AUTHOR, EMAIL);
+}
+
+
+/**
+ * Displays the version information.
+ */
+
+static void display_version(void) 
+{
+    printf("wl_unpackcursors %s\n", VERSION);
+    printf("\n%s\n", COPYRIGHT);
+    printf("This is free software; see the source for copying conditions. ");
+    printf("There is NO\nwarranty; not even for MERCHANTABILITY or FITNESS ");
+    printf("FOR A PARTICULAR PURPOSE.\n\nWritten by %s <%s>\n", AUTHOR, EMAIL);
+}
+
+
+/**
+ * Terminate the program with code 1 and the specified error message.
+ *
+ * @param message
+ *            The error message
+ */
+
+static void die(char *message, ...)
+{
+    va_list args;
+    
+    va_start(args, message);
+    vfprintf(stderr, message, args);
+    va_end(args);
+    exit(1);
+}
+
+
+/**
+ * Check options.
+ *
+ * @param argc
+ *            The number of arguments
+ * @param argv
+ *            The argument array
+ */
+
+static void check_options(int argc, char *argv[])
+{
+    char opt;
+    int index;
+    static struct option options[]={
+        {"help", 0, NULL, 'h'},
+        {"version", 0, NULL, 'V'}
+    };
+    
+    opterr = 0;
+    while((opt = getopt_long(argc, argv, "hV", options, &index)) != -1)
+    {
+        switch(opt) 
+        {
+            case 'V':
+                display_version();
+                exit(1);
+                break;
+                
+            case 'h':
+                display_usage();
+                exit(1);
+                break;
+                
+            default:
+                die("Unknown option: %s\nUse --help to show valid options.\n",
+                        argv[optind - 1]);
+        }
+    }
+}
+
+
+/**
+ * Writes a single sprite into the specified file in PNG format.
+ * 
+ * @param filename
+ *            The output filename
+ * @param cursors
+ *            The cursors
+ * @param spriteNo
+ *            The sprite number to write
+ */
+
+static void writePng(char *filename, wlImages cursors, int spriteNo)
+{
+    gdImagePtr output;
+    int x, y, i;
+    int palette[17];
+    int transparent;
+    int color;
+    FILE *file;
+
+    output = gdImageCreate(16, 16);
+    for (i = 0; i < 16; i++)
+    {
+        palette[i] = gdImageColorAllocate(output, wlPalette[i].red,
+                wlPalette[i].green, wlPalette[i].blue);
+    }
+    transparent = gdImageColorAllocate(output, 0, 0, 0);
+    gdImageColorTransparent(output, transparent);
+    for (y = 0; y < 16; y++)       
+    {
+        for (x = 0; x < 16; x++)
+        {
+            color = cursors[spriteNo][y * 16 + x];
+            gdImageSetPixel(output, x, y, color < 16 ? palette[color] : transparent);
+        }
+    }    
+    file = fopen(filename, "wb");
+    if (!file)
+    {
+        die("Unable to write PNG to %s: %s\n", filename, strerror(errno));
+    }
+    gdImagePng(output, file);
+    gdImageDestroy(output);
+    fclose(file);    
+}
+
+
+/**
+ * Writes all the cursors into the specified output directory.
+ * 
+ * @param outputDir
+ *            The output directory
+ * @param cursors
+ *            The cursors to write
+ */
+
+static void writePngs(char *outputDir, wlImages cursors)
+{
+    int i;
+    char *oldDir;
+    char filename[6];
+    
+    oldDir = getcwd(NULL, 0);
+    if (chdir(outputDir))
+    {
+        die("Unable to change to output directory %s: %s\n", outputDir,
+                strerror(errno));
+        return;
+    }
+    for (i = 0; i < 8; i++)
+    {
+        sprintf(filename, "%i.png", i);
+        writePng(filename, cursors, i); 
+    }
+    chdir(oldDir);
+    free(oldDir);
+}
+
+
+/**
+ * Main method
+ *
+ * @param argc
+ *            The number of arguments
+ * @param argv
+ *            The argument array
+ * @return Exit value
+ */
+
+int main(int argc, char *argv[])
+{  
+    char *filename, *outputDir;
+    wlImages cursors;
+    
+    /* Process options and reset argument pointer */
+    check_options(argc, argv);
+    argc -= optind;
+    argv += optind;
+    
+    /* Terminate if wrong number of parameters are specified */
+    if (argc != 2) die("Wrong number of parameters.\nUse --help to show syntax.\n");
+
+    /* Process parameters */
+    filename = argv[0];
+    outputDir = argv[1];
+    
+    /* Read the pic file */
+    cursors = wlCursorsReadFile(filename);
+    if (!cursors)
+    {
+        die("Unable to read cursors from %s: %s\n", filename,
+                strerror(errno));
+    }
+
+    /* Write the PNG files */
+    writePngs(outputDir, cursors);
+    
+    /* Free resources */
+    free(cursors);
+    
+    /* Success */
+    return 0;
+}
